e039a9ab1e1b603cd6f16d1a8be8e0b9b689aecc
Rashedul Hasan <Rashedul Hasan>
2021-09-15 00:09:15 +0600

    Simple React Part-02

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   const info = [
@@ -12,11 +13,16 @@ function App() {
   
   return (
     <div className="App">
-        <Person info = {info[0]}></Person>
+        <Users></Users>
+        <Count></Count>
+        {/* <Person info = {info[0]}></Person>
         <Person info = {info[1]}></Person>
         <Person info = {info[2]}></Person>
         <Person info = {info[3]}></Person>
-        <Person info = {info[4]}></Person>
+        <Person info = {info[4]}></Person> */}
+        {
+          info.map(information => <Person info = {information}></Person>)
+        }
     </div>
   );
 }
@@ -40,6 +46,43 @@ const tapu = {
   <h2>Job: {job}</h2>
   <h2>Salary: {salary}</h2>
   </div>
+}
+
+function Count(){
+  const [value, setValue] = useState(10);
+  return (
+    <div>
+      <h1>Value: {value}</h1>
+      <button onClick={()=>setValue(value+1)}>Increment</button>
+      <button onClick={()=>setValue(value-1)}>Decrement</button>
+    </div>
+  )
+}
+
+
+function Users(){
+  const [users, setUsers] = useState([]);
+  useEffect(()=>
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(data => setUsers(data))
+  )
+  return (
+    <div>
+          <h1>Total Users: {users.length}</h1>
+          <ul>
+            {
+              users.map(user => <li>{user.name}</li>)
+            }
+          </ul>
+
+          <ul>
+            {
+              users.map(user => <li>{user.email}</li>)
+            }
+          </ul>
+    </div>
+  )
 }
 
 export default App;
